b876b0797872870e8a455083f03ac8990ce95e11
THESHADOWKNIFE <168684371+THESHADOWKNIFE@users.noreply.github.com>
2025-07-16 12:59:13 +1200

    7/16

--- app.py
+++ app.py
@@ -1,16 +1,33 @@
 from flask_sqlalchemy import SQLAlchemy
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
+
+admin_r = Blueprint('admin',__name__, url_prefix='/admin')
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.debug = True
 
-app.config['SQLALCHEMY_DATABASE_URT'] = 'sqlite:///login.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///login.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy()
 db.init_app(app)
 app.register_blueprint(admin_r)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    password = db.Column(db.String(20))
+    email = db.Column(db.String(100), unique=True)
+
+class Quiz_result(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    right_number = db.Column(db.Integer)
+    quiz_number = db.Column(db.Integer)
+
+class Quiz_questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question = db.Column(db.String(255))
+    answer = db.Column(db.String(10))
 
 @app.route("/")
 def homepage():
